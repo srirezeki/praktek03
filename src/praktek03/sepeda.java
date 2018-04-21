d498957e8f7aa44fccf5002050bfc6439baa7bee
sri <sri12072>
2018-04-21 09:58:06 +0800

    buat method cetakinfo()

--- src/praktek03/sepeda.java
+++ src/praktek03/sepeda.java
@@ -4,4 +4,11 @@ public class sepeda {
  String merk;   
  String warna;
  double harga;
+ 
+ void cetakinfo(){
+     System.out.println("merk \t: "+merk+"\n"+
+             "warna \t: "+warna+"\n"+
+             "harga \t:"+harga);
+     
+ }
 }
