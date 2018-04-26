4ecc55b475551b762708333cb60acd7bd1e1598b
sri <sri12072>
2018-04-26 14:55:32 +0800

    instance,buat atribet dan panggil method

--- src/praktek03/sepedaaksi.java
+++ src/praktek03/sepedaaksi.java
@@ -1,4 +1,11 @@
 package praktek03;
 public class sepedaaksi {
+    public static void main(String[] args) {   
+    sepeda s = new sepeda();
+    s.merk="Kupu-kupu";
+    s.warna="Merah";
+    s.harga=1000;
     
+    s.cetakinfo();
+    }
 }
