65d7107d650a1eb6c1a2165300b931e755a5300e
sri <sri12072>
2018-04-26 15:37:16 +0800

    attribut dan method

--- src/praktek03/sepedamotor.java
+++ src/praktek03/sepedamotor.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
 
-/**
- *
- * @author My PC
- */
-public class sepedamotor {
-    
+public class sepedamotor extends sepeda {
+ private String jenismotor;
+ 
+ void throttle(){
+     System.out.printl("NGEEEENG......");
+ }
 }
